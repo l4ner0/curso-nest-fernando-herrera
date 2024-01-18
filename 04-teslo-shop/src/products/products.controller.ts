00c262ae565d7dc7d40eb6ec297f3294b301c899
Diego Urbina <diego.urbina@acceso.com.pe>
2024-01-18 11:48:15 -0500

    Auth en otros modulos

--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -13,12 +13,15 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -33,6 +36,7 @@ export class ProductsController {
     return this.productsService.findOnePlain(term);
   }
 
+  @Auth(ValidRoles.admin)
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -41,6 +45,7 @@ export class ProductsController {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @Auth(ValidRoles.admin)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
