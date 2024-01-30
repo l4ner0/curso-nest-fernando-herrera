5c41406500b49659419c38cf76abd0520c66fd14
Diego Urbina <diego.urbina@acceso.com.pe>
2024-01-30 14:28:14 -0500

    191. Encriptar contraseña

--- 04-teslo-shop/src/auth/entities/user.entity.ts
+++ 04-teslo-shop/src/auth/entities/user.entity.ts
@@ -1,8 +1,10 @@
+import { Product } from 'src/products/entities';
 import {
   BeforeInsert,
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -32,6 +34,12 @@ export class User {
     default: ['user'],
   })
   roles: string[];
+
+  @OneToMany(
+    () => Product,
+    (product) => product.user
+  )
+  product: Product;
 
   @BeforeInsert()
   checkFieldsBeforeInsert() {
